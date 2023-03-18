0a31f1c1083e5e8f2d07603e6479f513da4fd180
Aaron Hodgins <aaron.hodgins@nqry.com.au>
2023-03-18 14:54:08 +1030

    storage stuff

--- src/pages/game/[gameId]/index.tsx
+++ src/pages/game/[gameId]/index.tsx
@@ -1,41 +1,68 @@
 import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
 import { useEffect, useState } from "react";
-import { Problem, generateProblems } from "../../../components/numpad/Problem";
+import {
+  Problem,
+  generateProblems,
+  Operator,
+} from "../../../components/numpad/Problem";
 import { getStorageValueUnsafe } from "../../../utils/storage";
 
-const PROBLEM_STORAGE_KEY = "game-problems"
+const PROBLEM_STORAGE_KEY = "game-problems";
+
+interface QueryParams {
+  operator: Operator;
+  number: number;
+  gameId: string;
+}
+
+function parseQueryParams(query: ParsedUrlQuery): QueryParams {
+  // error handle if any null
+
+  const { operator, number, gameId } = query;
+  return {
+    operator: operator as Operator,
+    number: parseInt(number as string),
+    gameId: gameId as string,
+  };
+}
 
 const RunningGame = () => {
-    const [problems, setProblems] = useState<Problem[]>();
+  const [problems, setProblems] = useState<Problem[]>();
 
-    const router = useRouter();
-    const { operator, number, gameId } = router.query;
+  const router = useRouter();
 
-    useEffect(() => {
-        let currentProblems = getStorageValueUnsafe(getProblemsKey(gameId as string), "localStorage")
-        if (!currentProblems) {
-            currentProblems = generateProblems(number as number, operator as string);
-            localStorage.setItem(getProblemsKey(gameId as string), JSON.stringify(currentProblems))
-        }
+  const { operator, number, gameId } = parseQueryParams(router.query);
 
-        setProblems(currentProblems);
-    }, [])
-
-    function getProblemsKey(gameId: string) {
-        return `${PROBLEM_STORAGE_KEY}-${gameId}`
+  useEffect(() => {
+    let currentProblems: Problem[] | undefined = getStorageValueUnsafe(
+      getProblemsKey(gameId),
+      "localStorage"
+    );
+    if (!currentProblems) {
+      currentProblems = generateProblems(number, operator);
+      localStorage.setItem(
+        getProblemsKey(gameId),
+        JSON.stringify(currentProblems)
+      );
     }
-    
-    console.log(router)
 
-    return (
-        <div>
-            <h1>Running Game: {gameId}</h1>
-            <p>Operator: {operator}</p>
-            <p>Number: {number}</p>
-        </div>
-    )
-}
+    setProblems(currentProblems);
+  }, []);
 
-export {
-    RunningGame as default,
-}
+  function getProblemsKey(gameId: string) {
+    return `${PROBLEM_STORAGE_KEY}-${gameId}`;
+  }
+
+  console.log(router);
+
+  return (
+    <div>
+      <h1>Running Game: {gameId}</h1>
+      <p>Operator: {operator}</p>
+      <p>Number: {number}</p>
+    </div>
+  );
+};
+
+export { RunningGame as default };
