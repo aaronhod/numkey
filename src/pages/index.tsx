f3f4d5fb62bc488496f4d984f9ba6ed7eb7182fb
aaronhod <aaronhodgins2@gmail.com>
2024-04-13 19:24:10 +0930

    add quadrant root menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,60 @@
 import Head from "next/head";
 import React, { type ReactElement } from "react";
 import HeaderLayout from "@/components/layouts/HeaderLayout";
-import { SelectionScreen } from "@/components/views/SelectionScreen";
 import { type NextPageWithLayout } from "@/pages/_app";
+import { Button } from "@/components/shad-ui/button";
+import {
+  BookOpenText,
+  BrainCog,
+  Calculator,
+  Gamepad2,
+  type LucideIcon,
+} from "lucide-react";
+import { useRouter } from "next/router";
+
+interface SelectIconProps {
+  onClick: () => void;
+  Icon: LucideIcon;
+  title: string;
+}
+
+const SelectIcon = ({ onClick, Icon, title }: SelectIconProps) => (
+  <Button className="flex aspect-square h-32 w-32" onClick={onClick}>
+    <div className="flex flex-col items-center justify-center">
+      <Icon className="h-10 w-10" />
+      <span className="text-lg font-semibold">{title}</span>
+    </div>
+  </Button>
+);
+
+const QuadrantMenu = () => {
+  const router = useRouter();
+
+  return (
+    <main className="mt-24 grid grid-cols-2 gap-2 self-center">
+      <SelectIcon
+        onClick={() => router.push("/practice")}
+        Icon={Calculator}
+        title="Practice"
+      />
+      <SelectIcon
+        onClick={() => router.push("/game-smart")}
+        Icon={Gamepad2}
+        title="QuickPlay"
+      />
+      <SelectIcon
+        onClick={() => router.push("/game-custom")}
+        Icon={BookOpenText}
+        title="Custom"
+      />
+      <SelectIcon
+        onClick={() => router.push("/game-battle")}
+        Icon={BrainCog}
+        title="battle"
+      />
+    </main>
+  );
+};
 
 const Page: NextPageWithLayout = () => {
   return (
@@ -11,7 +63,7 @@ const Page: NextPageWithLayout = () => {
         <title>Math Game</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <SelectionScreen />
+      <QuadrantMenu />
     </>
   );
 };
