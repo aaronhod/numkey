7b34f80f385fb845f448691b08a6944b74303213
aaronhod <aaronhodgins2@gmail.com>
2024-08-04 18:48:08 +0930

    remove old structure

--- android/app/src/main/java/com/macaca/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.macaca.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.macaca.app',
-  appName: 'Macaca',
-  webDir: 'out'
-};
-
-export default config;
